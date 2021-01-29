e88c7d068bed298f343748cff1a526dcb14d97a7
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-29 15:16:13 +0100

    Extended Repository System + IMyRepositoryEntity interface ~ ParkingSpot now implements IMyRepositoryEntity ~ Ticket now implements IMyRepositoryEntity ~ Changed IMyRepository interface to be more generic ~ Restructured ParkingRepository to follow the new structure of IMyRepository ~ Restructured TicketRepository to follow the new structure of IMyRepository

---
+++ Oiski.School.ParkAndWash_H2_2021/Oiski.School.ParkAndWash_H2_2021/RepositorySystem/IMyRepositoryEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oiski.School.ParkAndWash_H2_2021
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="IDType">The type of the identifier for the <see cref="IMyRepositoryEntity{IDType, SaveType}"/></typeparam>
+    /// <typeparam name="SaveType">The type the <see cref="IMyRepositoryEntity{IDType, SaveType}"/> will save its state as or load a previous state from</typeparam>
+    public interface IMyRepositoryEntity<IDType, SaveType>
+    {
+        /// <summary>
+        /// The ID to identify the Entity in the <see cref="IMyRepository{EntityType, SaveType}"/>
+        /// </summary>
+        IDType ID { get; }
+
+        /// <summary>
+        /// Save the current state
+        /// </summary>
+        /// <returns>The current state of <see langword="this"/> <see cref="IMyRepositoryEntity{IDType, SaveType}"/> <see langword="object"/> as an instance of type <typeparamref name="SaveType"/></returns>
+        SaveType SaveEntity ();
+
+        /// <summary>
+        /// Restore a previous state based on the passed in <typeparamref name="SaveType"/> <see langword="value"/>
+        /// </summary>
+        /// <param name="_data"></param>
+        void BuildEntity (SaveType _data);
+    }
+}
